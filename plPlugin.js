cf51ed87fdf0342dbdfaea80e8e79351fa970866
Floofies <fantasticdragons@gmail.com>
2019-07-06 06:36:29 -0400

    Added hash

--- plPlugin.js
+++ plPlugin.js
@@ -4,15 +4,9 @@ const config = require('../config');
 if (!("plChat" in config)) throw new Error("presence-logger plugin not configured!");
 const composer = new Composer();
 function log(ctx, next) {
-	ctx.telegram.sendMessage(config.plChat, String(ctx.message.text + " " + ctx.chat.title));
-	next();
-}
-function configure(ctx, next) {
-	if (ctx.message.text)
-	const fs = require("fs");
-
+	const type = ctx.updateSubTypes[0] === "new_chat_members" ? "#joined" : "#left";
+	ctx.telegram.sendMessage(config.plChat, type + " " + ctx.message.text + " " + ctx.chat.title);
 	next();
 }
 composer.on([ 'new_chat_members', 'left_chat_member' ], log);
-composer.on([ 'text' ], configure);
 module.exports = composer;
